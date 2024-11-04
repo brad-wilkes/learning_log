461bef785b20f1590522c953d1a7ce5b2e21f979
brad-wilkes <rbwilkes@yahoo.com>
2024-11-04 11:10:30 -0600

    update local settings

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'learning_log_db',
+        'USER': 'learning_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
